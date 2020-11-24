9672d293ab78082cb68f3ee66bce7bac6c376ab9
Mikhail7G <60103038+Mikhail7G@users.noreply.github.com>
2020-11-24 21:10:53 +0500

    Добавьте файлы проекта.

---
+++ MFSDataGrabber/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace MFSDataGrabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ConnBtn = new System.Windows.Forms.Button();
+            this.SimConnectLbl = new System.Windows.Forms.Label();
+            this.TrueHeading = new System.Windows.Forms.Label();
+            this.DataUpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.Altitude = new System.Windows.Forms.Label();
+            this.WindPower = new System.Windows.Forms.Label();
+            this.Speed = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConnBtn
+            // 
+            this.ConnBtn.Location = new System.Drawing.Point(262, 222);
+            this.ConnBtn.Name = "ConnBtn";
+            this.ConnBtn.Size = new System.Drawing.Size(131, 38);
+            this.ConnBtn.TabIndex = 0;
+            this.ConnBtn.Text = "Connect";
+            this.ConnBtn.UseVisualStyleBackColor = true;
+            this.ConnBtn.Click += new System.EventHandler(this.ConnBtn_Click);
+            // 
+            // SimConnectLbl
+            // 
+            this.SimConnectLbl.AutoSize = true;
+            this.SimConnectLbl.Location = new System.Drawing.Point(290, 206);
+            this.SimConnectLbl.Name = "SimConnectLbl";
+            this.SimConnectLbl.Size = new System.Drawing.Size(78, 13);
+            this.SimConnectLbl.TabIndex = 1;
+            this.SimConnectLbl.Text = "Not connected";
+            this.SimConnectLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TrueHeading
+            // 
+            this.TrueHeading.AutoSize = true;
+            this.TrueHeading.Location = new System.Drawing.Point(476, 44);
+            this.TrueHeading.Name = "TrueHeading";
+            this.TrueHeading.Size = new System.Drawing.Size(35, 13);
+            this.TrueHeading.TabIndex = 2;
+            this.TrueHeading.Text = "label1";
+            // 
+            // DataUpdateTimer
+            // 
+            this.DataUpdateTimer.Interval = 1500;
+            this.DataUpdateTimer.Tick += new System.EventHandler(this.DataUpdateTimer_Tick);
+            // 
+            // Altitude
+            // 
+            this.Altitude.AutoSize = true;
+            this.Altitude.Location = new System.Drawing.Point(476, 72);
+            this.Altitude.Name = "Altitude";
+            this.Altitude.Size = new System.Drawing.Size(35, 13);
+            this.Altitude.TabIndex = 3;
+            this.Altitude.Text = "label1";
+            // 
+            // WindPower
+            // 
+            this.WindPower.AutoSize = true;
+            this.WindPower.Location = new System.Drawing.Point(122, 44);
+            this.WindPower.Name = "WindPower";
+            this.WindPower.Size = new System.Drawing.Size(35, 13);
+            this.WindPower.TabIndex = 4;
+            this.WindPower.Text = "label1";
+            // 
+            // Speed
+            // 
+            this.Speed.AutoSize = true;
+            this.Speed.Location = new System.Drawing.Point(476, 106);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(35, 13);
+            this.Speed.TabIndex = 5;
+            this.Speed.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Unispace", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(401, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Heading";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Unispace", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(455, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 14);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Plane";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Unispace", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(394, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Altitude";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(638, 295);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Speed);
+            this.Controls.Add(this.WindPower);
+            this.Controls.Add(this.Altitude);
+            this.Controls.Add(this.TrueHeading);
+            this.Controls.Add(this.SimConnectLbl);
+            this.Controls.Add(this.ConnBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Yellow;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnBtn;
+        private System.Windows.Forms.Label SimConnectLbl;
+        private System.Windows.Forms.Label TrueHeading;
+        private System.Windows.Forms.Timer DataUpdateTimer;
+        private System.Windows.Forms.Label Altitude;
+        private System.Windows.Forms.Label WindPower;
+        private System.Windows.Forms.Label Speed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MFSDataGrabber/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+
+
+namespace MFSDataGrabber
+{
+    public partial class Form1 : Form
+    {
+        // Компоненты сим коннекта.
+
+        private bool simConnectStatus;
+        private SimConnect simConn;
+
+        const int WM_USER_SIMCONNECT = 0x0402;
+
+        private enum DATA_STRUCT_ENUM
+        {
+            Plane,
+            World
+        }
+
+        private enum REQUEST_ENUM
+        {
+            planeReq,
+            worldReq
+                
+        }
+        private struct Plane
+        {
+            public double heading;
+            public double altitude;
+            public double speed;
+        }
+
+        private struct World
+        {
+            public double windPower;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitalComponents();
+        }
+
+
+        void InitalComponents()
+        {
+            simConnectStatus = false;
+            SimConnectLbl.Text = "Не подключено";
+
+            ConnectStatusChange();
+        }
+
+        void ConnectStatusChange()
+        {
+            SimConnectLbl.Text = simConnectStatus ? "Подключено" : "Не подключено";
+            ConnBtn.Text = simConnectStatus ? "Отключить" : "Подключить";
+        }
+
+        void Connect()
+        {
+
+            try
+            {
+                simConn = new SimConnect("MFSDataGrabber", this.Handle, WM_USER_SIMCONNECT, null, 0);
+                simConn.AddToDataDefinition(DATA_STRUCT_ENUM.Plane, "Plane Heading Degrees True", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simConn.AddToDataDefinition(DATA_STRUCT_ENUM.Plane, "Indicated Altitude", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simConn.AddToDataDefinition(DATA_STRUCT_ENUM.Plane, "AIRSPEED TRUE", "Knots", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+
+                simConn.AddToDataDefinition(DATA_STRUCT_ENUM.World, "AMBIENT WIND VELOCITY", "Knots", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+
+                simConn.RegisterDataDefineStruct<Plane>(DATA_STRUCT_ENUM.Plane);
+                simConn.RegisterDataDefineStruct<World>(DATA_STRUCT_ENUM.World);
+
+                simConn.OnRecvSimobjectDataBytype += OnResiveData;
+               
+                simConnectStatus = true;
+                DataUpdateTimer.Start();
+                ConnectStatusChange();
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        
+
+        void Disconnect()
+        {
+            try
+            {
+                simConn.Dispose();
+                simConnectStatus = false;
+                ConnectStatusChange();
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ConnBtn_Click(object sender, EventArgs e)
+        {
+            if(simConnectStatus)
+            {
+                Disconnect();
+            }
+            else
+            {
+                Connect();
+            }
+        }
+
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == 0x402)
+            {
+                if (simConn != null && simConnectStatus)
+                {
+                    simConn.ReceiveMessage();
+                }
+            }
+            else
+            {
+                base.DefWndProc(ref m);
+            }
+        }
+
+        private void DataUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            if(simConnectStatus)
+            {
+                simConn.RequestDataOnSimObjectType(REQUEST_ENUM.planeReq, DATA_STRUCT_ENUM.Plane,0,SIMCONNECT_SIMOBJECT_TYPE.USER);
+                simConn.RequestDataOnSimObjectType(REQUEST_ENUM.worldReq, DATA_STRUCT_ENUM.World, 0, SIMCONNECT_SIMOBJECT_TYPE.USER);
+
+            }
+        }
+
+        private void OnResiveData(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data)
+        {
+            if(data.dwRequestID == 0)
+            {
+                Plane recData = (Plane)data.dwData[0];
+                TrueHeading.Text = recData.heading.ToString();
+                Altitude.Text = recData.altitude.ToString();
+                Speed.Text = recData.speed.ToString();
+            }
+
+            if (data.dwRequestID == 1)
+            {
+                World recData = (World)data.dwData[0];
+                WindPower.Text = recData.windPower.ToString();
+            }
+        }
+    }
+}
